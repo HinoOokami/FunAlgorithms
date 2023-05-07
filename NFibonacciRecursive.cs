f341e774b64ee3fdf1b93759fc866785b4249360
Степан Лопатин <firewolf@mail.ru>
2023-05-07 19:19:46 +0300

    Add project files.

---
+++ NFibonacciRecursive.cs
@@ -0,0 +1,6 @@
+﻿class NFibonacciRecursive
+{
+    static void Run() => Console.WriteLine(Fibb(long.Parse(Console.ReadLine())));
+
+    static long Fibb(long n) => n <= 2 ? 1 : Fibb(n - 1) + Fibb(n - 2);
+}
---
+++ PalindromeOrNot.cs
@@ -0,0 +1,15 @@
+﻿class PalindromeOrNot
+{
+    static void Run()
+    {
+        var str = Console.ReadLine().ToLowerInvariant();
+        var isPal = true;
+        for (int fw = 0, bw = str.Length - 1; fw < str.Length / 2; fw++, bw--)
+            if (str[fw] != str[bw])
+            {
+                isPal = false;
+                break;
+            }
+        Console.WriteLine(isPal ? "palindrome" : "not a palindrome");
+    }
+}
